27234a6a917a4d3eaa9a60c6caf2d40bf5081a3f
pecheritsa <pecheritsa.roman@gmail.com>
2025-01-16 22:43:20 +0300

    add dev path

--- houses/urls.py
+++ houses/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from houses.views import HomePageView, HouseDetailView, HouseListView
+from houses.views import ComponentsPageView, HomePageView, HouseListView
 
 app_name = "houses"
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("dev/", ComponentsPageView.as_view(), name="dev"),
     path("houses/", HouseListView.as_view(), name="house_list"),
-    path("houses/<int:pk>/", HouseDetailView.as_view(), name="house_detail"),
+    # path("houses/<int:pk>/", HouseDetailView.as_view(), name="house_detail"),
 ]
