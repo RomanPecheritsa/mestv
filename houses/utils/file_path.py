e90d3b8d488e74fadb2151ab0e11215c1295297b
pecheritsa <pecheritsa.roman@gmail.com>
2025-01-29 00:38:45 +0300

    add utils path upload media

---
+++ houses/utils/file_path.py
@@ -0,0 +1,25 @@
+import os
+import uuid
+from django.utils.deconstruct import deconstructible
+
+
+@deconstructible
+class UploadToPath:
+    def __init__(self, sub_path):
+        self.sub_path = sub_path
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        filename = f"{uuid.uuid4().hex}.{ext}"
+        return os.path.join(self.sub_path, filename)
+
+
+def album_photo_path(instance, filename):
+    """Путь для фотографий в альбомах"""
+    return UploadToPath(f"houses/albums/album_{instance.album.id}")(instance, filename)
+
+
+def main_photo_path(instance, filename):
+    """Путь для главных фотографий моделей"""
+    model_name = instance._meta.model_name
+    return UploadToPath(f"houses/main_photo/{model_name}_main_photo")(instance, filename)
