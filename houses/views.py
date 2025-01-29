6e2b1020bf90154ff5160f94b5cc564cca14dd9d
pecheritsa <pecheritsa.roman@gmail.com>
2025-01-29 12:53:15 +0300

    add NewsListView

--- houses/views.py
+++ houses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.views import generic
 
-from houses.models import ContactInfo, HeaderText, House, Interior
+from houses.models import ContactInfo, HeaderText, House, Interior, News
 
 
 class HomePageView(generic.TemplateView):
@@ -15,8 +15,9 @@ class HomePageView(generic.TemplateView):
         data["header_texts"] = HeaderText.objects.all()
         data["contact_info"] = ContactInfo.objects.first()
 
-        data["main_houses"] = House.objects.all().order_by('?')[:4]
-        data["main_interiors"] = Interior.objects.all().order_by('?')[:3]
+        data["main_houses"] = House.objects.all().order_by("?")[:4]
+        data["main_interiors"] = Interior.objects.all().order_by("?")[:3]
+        data["main_news"] = News.objects.order_by("-created_at").first()
         return data
 
 
@@ -31,18 +32,25 @@ class HouseListView(generic.ListView):
     paginate_by = 8
 
 
+class NewsListView(generic.ListView):
+    model = News
+    queryset = News.objects.all()
+    context_object_name = "news_list"
+    paginate_by = 6
+
+
 class HouseDetailView(generic.DetailView):
     model = House
     queryset = House.objects.all()
-    context_object_name = 'house'
+    context_object_name = "house"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         house = self.get_object()
         if house.album:
-            context['photos'] = house.album.photos.all()
+            context["photos"] = house.album.photos.all()
         else:
-            context['photos'] = []
+            context["photos"] = []
         return context
 
 
@@ -64,5 +72,7 @@ class InteriorGalleryView(generic.ListView):
             active_interior = Interior.objects.first()
 
         context["active_interior"] = active_interior
-        context["photos"] = active_interior.album.photos.all() if active_interior.album else []
+        context["photos"] = (
+            active_interior.album.photos.all() if active_interior.album else []
+        )
         return context
