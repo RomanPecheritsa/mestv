05df0598c6b11142cf5011d15f83decc0945e020
pecheritsa <pecheritsa.roman@gmail.com>
2025-02-09 19:00:40 +0300

    linter module

--- houses/utils/file_path.py
+++ houses/utils/file_path.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+
 from django.utils.deconstruct import deconstructible
 
 
@@ -9,7 +10,7 @@ class UploadToPath:
         self.sub_path = sub_path
 
     def __call__(self, instance, filename):
-        ext = filename.split('.')[-1]
+        ext = filename.split(".")[-1]
         filename = f"{uuid.uuid4().hex}.{ext}"
         return os.path.join(self.sub_path, filename)
 
@@ -22,4 +23,6 @@ def album_photo_path(instance, filename):
 def main_photo_path(instance, filename):
     """Путь для главных фотографий моделей"""
     model_name = instance._meta.model_name
-    return UploadToPath(f"houses/main_photo/{model_name}_main_photo")(instance, filename)
+    return UploadToPath(f"houses/main_photo/{model_name}_main_photo")(
+        instance, filename
+    )
